cb0c5961c82333f0b3e23e01f1d60d23188ac0f2
Dylan Yang <banshiliuli1990@sina.com>
2019-02-19 10:50:56 +0800

    增加 dylan-*.el 的配置，增加 .gitignore 文件

---
+++ init.el
@@ -0,0 +1,48 @@
+(defun update-load-path (&rest _)
+  "Update `load-path'."
+  (push (expand-file-name "site-lisp" user-emacs-directory) load-path)
+  (push (expand-file-name "lisp" user-emacs-directory) load-path))
+(advice-add #'package-initialize :after #'update-load-path)
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+
+(update-load-path)
+
+;; Set package archives
+(setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
+			 ("melpa" . "http://elpa.emacs-china.org/melpa/")
+			 ("org" . "http://elpa.emacs-china.org/org/")))
+
+;; Initialize packages
+(unless (bound-and-true-p package--initialized)
+  (setq package-enable-at-startup nil)
+  (package-initialize))
+
+;; use package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+;; Should set before loading `use-package'
+
+(setq use-package-always-ensure t)
+(setq use-package-always-defer t)
+(setq use-package-expand-minimally t)
+(setq use-package-enable-imenu-support t)
+
+(eval-when-compile
+  (require 'use-package))
+
+(use-package diminish)
+(use-package bind-key)
+
+(use-package package-utils
+	     :init
+	     (defalias 'upgrade-packages 'package-utils-upgrade-all)
+	     (defalias 'upgrade-packages-and-restart 'package-utils-upgrade-all-and-restart))
+
+(declare-function upgrade-packages-and-restart 'init-package)
+(defalias 'dylan-update-packages-and-restartup 'upgrade-packages-and-restart)
+
+(require 'dylan-basic)
+(require 'dylan-org)
+(require 'dylan-magit)
---
+++ lisp/dylan-magit.el
@@ -0,0 +1,33 @@
+(use-package magit
+  :bind (("C-x g" . magit-status))
+  :config
+  (when sys/winntp
+    (setenv "GIT_ASKPASS" "git-gui--askpass"))
+
+  ;; Access Git forges from Magit
+  ;; https://github.com/skeeto/emacsql#windows-issues
+  (unless sys/winntp
+    (use-package forge :demand))
+
+  ;; Show tasks
+  (use-package magit-todos
+    :init (magit-todos-mode 1)))
+
+;; Walk through git revisions of a file
+(use-package git-timemachine
+  :bind (:map vc-prefix-map
+              ("t" . git-timemachine)))
+
+;; Highlighting regions by last updated time
+(use-package smeargle
+  :bind (:map vc-prefix-map
+              ("S" . smeargle)
+              ("C" . smeargle-commits)
+              ("R" . smeargle-clear)))
+
+;; Git related modes
+(use-package gitattributes-mode)
+(use-package gitconfig-mode)
+(use-package gitignore-mode)
+
+(provide 'dylan-magit)
