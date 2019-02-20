067666b1faf61460b5432076808480780e20b309
Dylan Yang <banshiliuli1990@sina.com>
2019-02-20 15:43:40 +0800

    配置完成了 Org Mode 和 elfeed 的配置

---
+++ lisp/dylan-expansion.el
@@ -0,0 +1,24 @@
+;; Emacs development
+(use-package elisp-mode
+  :ensure nil
+  :bind (:map emacs-lisp-mode-map
+              ("C-c C-x" . ielm)
+              ("C-c C-c" . eval-defun)
+              ("C-c C-b" . eval-buffer)))
+
+(use-package eldoc
+  :ensure nil
+  :diminish eldoc-mode)
+
+;; Emacs reading
+(use-package elfeed
+  :bind ("C-x w e" . elfeed)
+  :config
+  (setq elfeed-db-directory (expand-file-name ".elfeed" user-emacs-directory)
+        elfeed-feeds
+        '("http://planet.emacsen.org/atom.xml"
+          "http://www.masteringemacs.org/feed/"
+          "https://oremacs.com/atom.xml"
+          "https://pinecast.com/feed/emacscast")))
+
+(provide 'dylan-expansion)
