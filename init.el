945dc6bedeabfb021e342788b29a314c3356ef57
Dylan Yang <banshiliuli1990@sina.com>
2019-02-21 16:53:52 +0800

    集中配置，移除无用内容。
    1. 将 use-package 的配置放到 dylan-package.el 文件中
    2. 将菜单栏等配置放到 dylan-basic.el 中

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
+;;; init.el --- summary -*- lexical-binding: t -*-
+
+;; Author: Dylan Yang
+;; Maintainer: Dylan Yang
+;;; Commentary:
+
+;;; Code:
 (defun update-load-path (&rest _)
   "Update `load-path'."
   (push (expand-file-name "site-lisp" user-emacs-directory) load-path)
@@ -8,45 +15,13 @@
 
 (update-load-path)
 
-;; Set package archives
-(setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
-			 ("melpa" . "http://elpa.emacs-china.org/melpa/")
-			 ("org" . "http://elpa.emacs-china.org/org/")))
-
-;; Initialize packages
-(unless (bound-and-true-p package--initialized)
-  (setq package-enable-at-startup nil)
-  (package-initialize))
-
-;; use package
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-;; Should set before loading `use-package'
-
-(setq use-package-always-ensure t)
-(setq use-package-always-defer t)
-(setq use-package-expand-minimally t)
-(setq use-package-enable-imenu-support t)
-
-(eval-when-compile
-  (require 'use-package))
-
-(use-package diminish)
-(use-package bind-key)
-
-(use-package package-utils
-	     :init
-	     (defalias 'upgrade-packages 'package-utils-upgrade-all)
-	     (defalias 'upgrade-packages-and-restart 'package-utils-upgrade-all-and-restart))
-
-(declare-function upgrade-packages-and-restart 'init-package)
-(defalias 'dylan-update-packages-and-restartup 'upgrade-packages-and-restart)
-
+(require 'dylan-package)
 (require 'dylan-basic)
 (require 'dylan-org)
 (require 'dylan-magit)
 (require 'dylan-expansion)
 (require 'dylan-company)
+(require 'dylan-smex)
 
 (provide 'init)
+;;; init.el ends here
