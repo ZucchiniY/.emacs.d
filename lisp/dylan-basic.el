21a5d9a46e0b8a6dcd9eb49f9b37deca06d624e3
Dylan Yang <banshiliuli1990@sina.com>
2019-02-25 17:06:43 +0800

    移除 Emacs Multiple Cursors ，使用 Evil-Mc 启用 Evil

--- lisp/dylan-basic.el
+++ lisp/dylan-basic.el
@@ -156,18 +156,5 @@ CHINESE-SIZE is the chinese font size."
   :diminish undo-tree-mode
   :hook (after-init . global-undo-tree-mode))
 
-;; Multiple cursors
-(use-package multiple-cursors
-  :bind (("C-S-c C-S-c"   . mc/edit-lines)
-         ("C->"           . mc/mark-next-like-this)
-         ("C-<"           . mc/mark-previous-like-this)
-         ("C-c C-<"       . mc/mark-all-like-this)
-         ("C-M->"         . mc/skip-to-next-like-this)
-         ("C-M-<"         . mc/skip-to-previous-like-this)
-         ("s-<mouse-1>"   . mc/add-cursor-on-click)
-         ("C-S-<mouse-1>" . mc/add-cursor-on-click)
-         :map mc/keymap
-         ("C-|" . mc/vertical-align-with-space)))
-
 (provide 'dylan-basic)
 ;;; dylan-basic.el ends here
---
+++ lisp/dylan-evil.el
@@ -0,0 +1,53 @@
+;;; dylan-evil.el --- summary -*- lexical-binding: t -*-
+
+;; Author: Dylan Yang
+;; Maintainer: Dylan Yang
+;;; Commentary:
+
+;;; Code:
+(use-package evil
+  :hook (after-init . evil-mode)
+  :init (setq evil-want-keybinding nil)
+  :config
+  (setq evil-default-state 'normal)
+  ;; 将不希望使用 Evil Mode 的内容，放在这里
+  (evil-set-initial-state 'dired-mode 'emacs)
+  ;; evil ex command `:W' to save all buffers.
+  (evil-ex-define-cmd "W" 'evil-write-all)
+  
+  ;; evil normal state keybinds
+  (define-key evil-normal-state-map "Y" (kbd "y$")))
+
+;; gcc comments out a line
+;; gc comments out the target of a motion
+;; gcap comments out a paragraph
+(use-package evil-commentary
+  :after evil
+  :diminish evil-commentary-mode
+  :init (evil-commentary-mode))
+
+;; evil surround
+(use-package evil-surround
+  :after evil
+  :init (global-evil-surround-mode))
+
+(use-package evil-matchit
+  :after evil
+  :diminish evil-matchit-mode
+  :init (setq evilmi-ignore-comments nil)
+  :config (global-evil-matchit-mode t))
+
+(use-package evil-mc
+  :after evil
+  :diminish evil-mc-mode
+  :hook (after-init . global-evil-mc-mode)
+  :commands (evil-mc-make-cursor-move-next-line
+             evil-mc-make-cursor-move-prev-line
+             evil-mc-mode
+             evil-mc-undo-all-cursors
+             global-evil-mc-mode)
+  :init (global-evil-mc-mode 1))
+
+(provide 'dylan-evil)
+
+;;; dylan-evil.el ends here
