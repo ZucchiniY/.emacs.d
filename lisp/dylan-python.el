63e0ed08712b1b3e2fae9efd1c9754060f348887
dylanYang <banshiliuli1990@sina.com>
2019-02-26 22:15:27 +0800

    重新配置 python mode 使其可以补全

--- lisp/dylan-python.el
+++ lisp/dylan-python.el
@@ -8,17 +8,22 @@
 
 (use-package python-mode
   :mode ("\\.py\\'" . python-mode)
-  :config
-  (use-package elpy
-    :init
-    (elpy-enable)
-    (when (require 'flycheck nil t)
-      (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-      (add-hook 'elpy-mode-hook 'flycheck-mode))
-    (setq python-shell-interpreter "python"
+  :defines python-indent-offset
+  :config (setq python-indent-offset 4))
+
+(use-package elpy
+  :after python
+  :init
+  (elpy-enable)
+  (when (require 'flycheck nil t)
+    (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+    (add-hook 'elpy-mode-hook 'flycheck-mode))
+  (setq python-shell-interpreter "python"
         python-shell-interpreter-args "-i"))
-  (use-package py-autopep8
-    :hook (elpy-mode . py-autopep8-enable-on-save)))
+
+(use-package py-autopep8
+  :after python
+  :hook (elpy-mode . py-autopep8-enable-on-save))
 
 (use-package quickrun
   :bind ("C-c x" . quickrun))
