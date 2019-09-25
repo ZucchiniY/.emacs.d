050857feb6bd70586f2b61277e72b480d9702b80
Dylan Yang <banshiliuli1990@sina.com>
2019-09-25 10:30:06 +0800

    :bug: 修改 python 不能执行问题

--- modules/modules-python.el
+++ modules/modules-python.el
@@ -1,7 +1,6 @@
 (use-package python
   :ensure nil
-  :config (setq python-shell-completion-native-enable nil
-                python-indent-offset 4))
+  :config (setq python-shell-completion-native-enable nil))
 
 (use-package elpy
   :after python
@@ -10,13 +9,14 @@
   (when (require 'flycheck nil t)
     (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
     (add-hook 'elpy-mode-hook 'flycheck-mode))
-  (setq elpy-rpc-python-command "python3")
   (setq python-shell-interpreter "python3"
         python-shell-interpreter-args "--pylab=osx --pdb --nosep --classic"
-        python-shell-prompt-regexp ">>>"
+        python-shell-prompt-regexp ">>> "
         python-shell-prompt-output-regexp ""
         python-shell-completion-setup-code "from IPython.core.completerlib import module_completion"
-        python-shell-completion-string-code "';'.join(get_ipython().Completer.all_completions('''%s'''))\n"))
+        python-shell-completion-string-code "';'.join(module_completion('''%s'''))\n"
+        python-shell-completion-string-code "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+  (setq elpy-rpc-python-command "python3"))
 
 (use-package py-autopep8
   :after python
