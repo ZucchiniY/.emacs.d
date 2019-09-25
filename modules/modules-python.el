9d9ae2ba21000f7d0a41ee3494ab22e6e2a80b81
Dylan Yang <banshiliuli1990@sina.com>
2019-09-25 16:58:25 +0800

    :zap: 移除复杂的 elpy , 使用 anaconda-mode 代替

--- modules/modules-python.el
+++ modules/modules-python.el
@@ -1,37 +1,16 @@
-(use-package python
-  :ensure nil
-  :config (setq python-shell-completion-native-enable nil))
-
-(use-package elpy
-  :after python
+(use-package anaconda-mode
+  :commands anaconda-mode
+  :general
+  (global-leader
+    "pd" 'anaconda-mode-find-definitions)
   :init
-  (elpy-enable)
-  (when (require 'flycheck nil t)
-    (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-    (add-hook 'elpy-mode-hook 'flycheck-mode))
-  (setq python-shell-interpreter "python3"
-        python-shell-interpreter-args "--pylab=osx --pdb --nosep --classic"
-        python-shell-prompt-regexp ">>> "
-        python-shell-prompt-output-regexp ""
-        python-shell-completion-setup-code "from IPython.core.completerlib import module_completion"
-        python-shell-completion-string-code "';'.join(module_completion('''%s'''))\n"
-        python-shell-completion-string-code "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
-  (setq elpy-rpc-python-command "python3"))
+  (add-hook 'python-mode-hook #'anaconda-mode)
+  (add-hook 'python-mode-hook #'anaconda-eldoc-mode))
 
-(use-package pipenv
-  :hook (python-mode . pipenv-mode)
-  :init (setq pipenv-projectile-after-switch-function #'pipenv-projectile-after-switch-extended))
-
-(use-package pyenv-mode
-  :hook (python-mode . pyenv-mode))
-(use-package py-isort
-  :hook (before-save . py-isort-before-save))
-(use-package blacken
-  :hook (python-mode . blacken-mode)
-  :config (setq blacken-skip-string-normallization t))
-(use-package py-autopep8
-  :after python
-  :hook (elpy-mode . py-autopep8-enable-on-save))
+(use-package company-anaconda
+  :after company
+  :init
+  (add-to-list 'company-backends '(company-anaconda :with company-capf)))
 
 (use-package quickrun
   :general
