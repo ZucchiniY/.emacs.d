9fd7f5859ad0c705b0d7ecf16a8b1251911d932d
Dylan Yang <banshiliuli1990@sina.com>
2019-12-02 10:26:04 +0800

    :sparkles: : 增加 org mode 生成 graphviz-dot 文件方案
    增加生成配置 modules-mind-map ，不过生成的图片比较丑，不如幕布好看。
    将启用配置增加到 init.el 中
    
    修改：     init.el
    新文件：   modules/modules-mind-map.el

---
+++ modules/modules-mind-map.el
@@ -0,0 +1,15 @@
+(use-package org-mind-map
+  :init
+  (require 'ox-org)
+  :ensure t
+  :config
+  (setq org-mind-map-engine "dot")       ; Default. Directed Graph
+  ;; (setq org-mind-map-engine "neato")  ; Undirected Spring Graph
+  ;; (setq org-mind-map-engine "twopi")  ; Radial Layout
+  ;; (setq org-mind-map-engine "fdp")    ; Undirected Spring Force-Directed
+  ;; (setq org-mind-map-engine "sfdp")   ; Multiscale version of fdp for the layout of large graphs
+  ;; (setq org-mind-map-engine "twopi")  ; Radial layouts
+  ;; (setq org-mind-map-engine "circo")  ; Circular Layout
+  )
+
+(provide 'modules-mind-map)
