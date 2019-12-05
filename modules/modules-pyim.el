7437d09dd43c89da32e596e75212f9a68cacecf5
Dylan Yang <banshiliuli1990@sina.com>
2019-12-05 11:05:37 +0800

    :sparkles: : 增加支持在 emacs 中使用 pyim+rime
    在 Emacs 中使用 pyim 进行写，可以控制相关的模式问题，解决
    外部输入法的引入，切换等问题。
    
    修改：     .gitignore
    修改：     README.org
    修改：     init.el
    新文件：   liberime.so
    新文件：   modules/modules-pyim.el

---
+++ modules/modules-pyim.el
@@ -0,0 +1,35 @@
+(use-package pyim
+  :demand t
+  :diminish pyim-isearch-mode
+  :init
+  (setq default-input-method "pyim"
+        pyim-default-scheme 'rime
+        pyim-page-length 7
+        pyim-page-tooltip 'proframe)
+  :config
+  (setq-default pyim-english-input-switch-functions
+                '(pyim-probe-dynamic-english
+                  pyim-probe-evil-normal-mode
+                  pyim-probe-program-mode
+                  pyim-probe-org-structure-template))
+
+  (setq-default pyim-punctuation-half-width-functions
+                '(pyim-probe-punctuation-line-beginning
+                  pyim-probe-punctuation-after-punctuation))
+  (pyim-isearch-mode t)
+  :bind ("M-j" . pyim-convert-string-at-point))
+
+(use-package liberime
+  :load-path (lambda () (expand-file-name "rime" user-emacs-directory))
+  :custom
+  (rime_share_data_dir "/Library/Input Methods/Squirrel.app/Contents/SharedSupport/")
+  (rime_user_data_dir (expand-file-name "rime" user-emacs-directory))
+  :init
+  (module-load (expand-file-name "liberime.so" user-emacs-directory))
+  :config
+  (liberime-start rime_share_data_dir rime_user_data_dir)
+  (liberime-select-schema "wubi86"))
+
+(use-package posframe)
+
+(provide 'modules-pyim)
