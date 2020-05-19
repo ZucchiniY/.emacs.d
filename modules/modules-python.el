f0de4bcc2bb0b99f5a9e422c1f1bef3699b08c47
Dylan Yang <banshiliuli1990@sina.com>
2020-05-19 15:36:13 +0800

    :sparkles: 更新 .gitignore 和 core 及 modules 配置
    1. 补充忽略 .lsp-session-v1 目录
    2. 补充忽略 extends/mspyls/ 目录
    3. 补充忽略 Python-Language-server.nupkg 文件
    4. 更新 counsel 配置，重新引入 swiper
    5. 更新 lsp 和 python 配置，使用 lsp + mspyls 做为开发平台
    6. 更新 Web 配置，引入 jinja2 模板
    7. 补充对应的 Readme.org 文档
    
    针对 mspyls 说明:
    
    在不同操作系统下需要不同的文件设置，帮将配置加入忽略目录中，在不同系统下，引用不同的配置方案。
    
    修改：     .gitignore
    修改：     README.org
    修改：     core/core-counsel.el
    修改：     init.el
    新文件：   modules/modules-lsp.el
    修改：     modules/modules-python.el
    修改：     modules/modules-web.el

--- modules/modules-python.el
+++ modules/modules-python.el
@@ -1,15 +1,38 @@
-(use-package anaconda-mode
-  :commands anaconda-mode
-  :diminish (anaconda-eldoc-mode anaconda-mode)
-  :general
-  (global-leader
-    "pd" 'anaconda-mode-find-definitions)
-  :init
-  (add-hook 'python-mode-hook #'anaconda-mode))
+(use-package exec-path-from-shell :defer t)
 
-(use-package company-anaconda
-  :after company
+(use-package python
+  :mode ("\\.py\\'" . python-mode)
   :init
-  (add-to-list 'company-backends '(company-anaconda :with company-capf)))
+  (setq python-shell-completion-native-enable nil)
+  (setq-default python-indent-offset 4
+                python-indent-guess-indent-offset-verbose nil)
+  :commands (python-mode run-python)
+  ;; :intercept ("python3" . python-mode)
+  :config
+  (with-eval-after-load 'exec-path-from-shell
+    (exec-path-from-shell-copy-env "PYTHONPATH")))
+
+(use-package py-isort
+  :init
+  (setq python-sort-imports-on-save t)
+  (defun +python/python-sort-imports ()
+    (interactive)
+    (when (and python-sort-imports-on-save
+               (derived-mode-p 'python-mode))
+      (py-isort-before-save)))
+  (add-hook 'python-mode-hook
+            (lambda () (add-hook 'before-save-hook #'+python/python-sort-imports))))
+
+(use-package lsp-python-ms
+  :hook (python-mode . (lambda ()
+                        (require 'lsp-python-ms) (lsp-deferred)))
+  :after lsp-mode python
+  :custom
+  (lsp-python-ms-dir (expand-file-name "extends/mspyls/" user-emacs-directory))
+  ;; :init
+  ;; (setq lsp-python-ms-executable (expand-file-name "extends/mspyls/Microsoft.Python.LanguageServer" user-emacs-directory))
+  ;; (executable-find lsp-python-ms-executable)
+  )
+
 
 (provide 'modules-python)
