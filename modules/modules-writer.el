c3c756d935fdb7aa4909fc75d387d68d1a8c3d28
Zucchiniy <banshiliuli1990@sina.com>
2022-03-02 00:01:19 +0800

    :wrench: 新增 pangu-spacing 包，辅助增加空格
    修改：     modules/modules-writer.el

--- modules/modules-writer.el
+++ modules/modules-writer.el
@@ -2,10 +2,11 @@
 
 ;; Author: Dylan Yang
 ;; Maintainer: Dylan Yang
-;; Version: 1.0.0
+;; Version: 2.0.0
 ;; Package-Requires: ()
 
 ;;; Commentary:
+;; feature: 新增 pangu-spacing 包，用来控制空格的显示
 
 ;;; Code:
 
@@ -14,6 +15,15 @@
   (setq writeroom-extra-line-spacing 5
         writeroom-width 0.5)
   (advice-add 'writeroom--calculate-width :before #'redisplay))
+
+(use-package pangu-spacing
+  :init
+  (global-pangu-spacing-mode t)
+  (setq pangu-spacing-real-insert-separtor t)
+  :config
+  (add-hook 'org-mode-hook
+            '(lambda ()
+               (set (make-local-variable 'pangu-spacing-real-insert-separtor) t))))
 
 (provide 'modules-writer)
 
