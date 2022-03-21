2dc7d1ef8dcb6c42fe571f51d8182bbb24350ece
Zucchiniy <banshiliuli1990@sina.com>
2022-03-21 23:59:13 +0800

    :art: 增加 init 文件的说明
    修改：     init.el

--- init.el
+++ init.el
@@ -1,3 +1,36 @@
+;;; init.el --- summary -*- lexical-binding: t -*-
+
+;; Author: Dylan Yang
+;; Maintainer: Dylan Yang
+;; Version: 1.0.0
+;; Package-Requires: ()
+;; Homepage: 
+;; Keywords: 
+
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; 初始化文件
+
+;;; Code:
+
+
 (defun update-load-path (&rest _)
   "Update `load-path'."
   (push (expand-file-name "core" user-emacs-directory) load-path)
@@ -58,3 +91,4 @@
 
 (provide 'init)
 ;;; init.el ends here
+
