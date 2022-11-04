73fff87f963718fb1fd48505fc3405d0eca1b88b
Zucchini <banshiliuli1990@sina.com>
2022-11-04 10:22:50 +0800

    :recycle: refactor abbrev-mode config
    修改：     core/core-package.el

--- core/core-package.el
+++ core/core-package.el
@@ -24,9 +24,13 @@
   (add-to-list 'load-path "site-lisp/use-package")
   (require 'use-package))
 
-;; abbrev-mode
-(add-hook 'abbrev-mode-hook (lambda () (diminish 'abbrev-mode)))
-
+;; abbrev-mode abbreviation file-name
+(use-package abbrev
+  :diminish abbrev-mode
+  :config
+  (if (file-exists-p abbrev-file-name)
+      (quietly-read-abbrev-file)))
+                     
 ;; add all-the-icons package
 (use-package all-the-icons
   :load-path "site-lisp/all-the-icons"
@@ -70,67 +74,6 @@
   :defer t
   :config
   (recentf-mode t))
-
-(use-package general
-  :defer 1
-  :commands general-override-states
-  :init
-  (setq general-override-states '(insert
-                                  emacs
-                                  hybrid
-                                  normal
-                                  visual
-                                  motion
-                                  operator
-                                  replacea))
-  :config
-  (general-evil-setup t)
-  (general-create-definer global-leader
-    :states '(normal visual motion)
-    :prefix "SPC"
-    :keymaps 'override)
-  (general-create-definer local-leader
-    :states '(normal visual motion)
-    :prefix "SPC m"
-    :keymaps 'override)
-  (global-leader
-    ;; files keybinds
-    "fs" 'save-buffer
-    "fd" 'dired
-    ;; buffer keybinds
-    "bk" 'kill-buffer
-    ;; quite emacs
-    "qq" 'save-buffers-kill-emacs
-    ;; winner
-    "wu" 'winner-undo
-    "wr" 'winner-redo
-    ;; window keybinds
-    "wo" 'other-window
-    "wv" 'split-window-vertically
-    "w-" 'split-window-horizontally
-    "wl" 'evil-window-right
-    "wh" 'evil-window-left
-    "wk" 'evil-window-up
-    "wj" 'evil-window-down
-    "wq" 'delete-window
-    "wa" 'delete-other-windows
-    ;; windows select
-     "1" 'winum-select-window-1
-     "2" 'winum-select-window-2
-     "3" 'winum-select-window-3
-     "4" 'winum-select-window-4
-     "5" 'winum-select-window-5
-     "6" 'winum-select-window-6
-     "7" 'winum-select-window-7
-     "8" 'winum-select-window-8
-     "9" 'winum-select-window-9
-     "0" 'winum-select-window-0-or-10
-     ;; writeroom
-     "rw" 'writeroom-mode
-     ;; cfw::open-org-calendar
-     "ov" 'cfw:open-org-calendar
-    ))
-
 
 (provide 'core-package)
 ;;; core-package.el ends here
