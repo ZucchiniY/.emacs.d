7ec715a9704ed6bfb063db8c7ffc93d75fb4753f
Zucchiniy <banshiliuli1990@sina.com>
2022-11-25 11:52:25 +0800

    :wrench: 修改 dap-mode 配置 dap-python
    修改：     modules/modules-lsp-python.el

--- modules/modules-lsp-python.el
+++ modules/modules-lsp-python.el
@@ -9,13 +9,35 @@
   :commands (lsp lsp-deferred))
 
 ;; debugger
-(use-package dap-mode)
-(use-package dap-python)
-;; (use-package dap-javascript)
-;; (use-package dap-typescript)
+(use-package dap-mode
+  :after lsp-mode
+  :commands (dap-debug dap-register-debug-template)
+  :hook ((python-mode . dap-ui-mode)
+         (python-mode . dap-mode))
+  :custom
+  (dap-auto-configure-mode)
+  (dap-auto-configure-feature
+   '(sessions locals breakpoints expressions tooltip))
+  :config
+  (require 'dap-python)
+  ;; (require 'dap-javascript)
+  ;; (require 'dap-typescript)
+  (dap-register-debug-template
+   "Python dap"
+   (list :type "python"
+         :args "-i"
+         :cwd nil
+         :env '(("DEBUG" . "1"))
+         ;; :target-module (expand-file-name "")
+         :request "launch"
+         ;; :name "
+
+         )
+   )
+  )
 
 ;; treemacs and ivy
-(use-package lsp-treemacs)
+(use-package lsp-treemacs :config (lsp-treemacs-sync-mode 1))
 (use-package lsp-ivy)
 
 ;; lsp-python
