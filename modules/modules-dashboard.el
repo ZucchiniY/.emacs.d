3ceafa0b94fc3da1f78abd40fd7cc423dcc5762d
Dylan <banshiliuli1990@sina.com>
2025-03-12 00:42:46 +0800

    :wrench: 更新 dashboard 配置
    要提交的变更：
    	修改：     modules/modules-dashboard.el

--- modules/modules-dashboard.el
+++ modules/modules-dashboard.el
@@ -14,22 +14,23 @@
 (use-package dashboard
   :ensure t
   :init
-  ;; :config
+  (setq dashboard-projects-backend 'projectile)
   (setq dashboard-set-heading-icons t
         dashboard-set-file-icons t
-        dashboard-set-init-info t
         dashboard-center-content t
         dashboard-banner-logo-title "Dylan's Emacs - Talk is cheap. Show me the code."
         dashboard-startup-banner (expand-file-name "logo.png" user-emacs-directory)
-        dashboard-items '((recents . 5)
-                          (projects . 5)
-                          (bookmarks . 5)))
+        dashboard-items '((recents   . 5)
+                          (projects  . 5)
+                          (bookmarks . 5)
+                          (agenda    . 5)
+                          (registers . 5)
+                          ))
+  (setq dashboard-projects-switch-function 'counsel-projectile-switch-project)
 
-  (setq dashboard-icon-type 'nerd-icons)
-  
-  (setq dashboard-display-icons-p t
-        dashboard-set-navigator t
-        dashboard-set-footer nil)
+  ;; nerd icons
+  (setq dashboard-icon-type 'nerd-icons
+        dashboard-display-icons-p t)
 
   (setq dashboard-week-agenda t
         dashboard-filter-agenda-entry 'dashboard-no-filter-agenda)
