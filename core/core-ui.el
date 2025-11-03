a0e2e26441251bc5b3b6cb434f3c40b06cca830b
Dylan <banshiliuli1990@sina.com>
2025-11-04 00:19:30 +0800

    :wrench: 更新中文字体为 霞鹜文楷
    要提交的变更：
    	修改：     core/core-ui.el

--- core/core-ui.el
+++ core/core-ui.el
@@ -27,7 +27,8 @@
 
 
 (cond (sys/mac-x-p (dylan//set-monospaced-font "Iosevka Nerd Font Mono" "华文仿宋" 14 14))
-      (sys/linux-x-p (dylan//set-monospaced-font "Iosevka Nerd Font Mono" "Wenquanyi Micro Hei Mono" 18 18))
+      ;; (sys/linux-x-p (dylan//set-monospaced-font "Iosevka Nerd Font Mono" "Wenquanyi Micro Hei Mono" 18 18))
+      (sys/linux-x-p (dylan//set-monospaced-font "Iosevka Nerd Font Mono" "LXGW WenKai Mono Medium" 18 18))
       (sys/win-x-p (dylan//set-monospaced-font "Iosevka Nerd Font Mono" "Microsoft YaHei" 14 14)))
 
 (use-package doom-modeline
